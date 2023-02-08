89e52b245134a8a1af37aebdb890e4b666b70ab6
chaseknis <chase6th@gmail.com>
2023-02-08 10:32:01 +0200

    Display list of items on the Homepage to resolve #15

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import './style.css';
 
-const movies = [1,1,1,1,1,1]
-const coontainer = document.querySelector(".movies_container")
+const container = document.querySelector('.movies_container');
 
-const populate_ui = ()=>{
-    const maindiv = document.createElement("div")
-    const div = document.createElement("div")
-    div.classList.add("movie_wrapper")
-    div.innerHTML = `
+const populateUI = (data) => {
+  const maindiv = document.createElement('div');
+  maindiv.classList.add('maincard');
+  const div = document.createElement('div');
+  div.classList.add('movie_wrapper');
+  div.innerHTML = `
     <div class="poster_wrapper">
-        <img src="https://static.tvmaze.com/uploads/images/medium_portrait/439/1098374.jpg" alt="">
+        <img src=${data.image.medium} alt="">
         <div class="details_wrapper">
-        <p>Movie name</p>
-        <div>Likes</div>
+        <p>${data.name}</p>
+        <div id=${data.id} class="likewrap"><img class="heart" src="https://img.icons8.com/color/48/ffffff/hearts.png"/></div>
         </div>
     </div>
-    <div class="buttons_Wrapper">
-        <button>Comments</button>
+    <div class="buttons_Wrapper" id=${data.id}>
+        <button id=${data.id} class="comment">Comments</button>
     </div>
-    `
-   movies.forEach((data)=>{
-    maindiv.appendChild(div)
-   }) 
-
-   coontainer.append(maindiv)
-}
-populate_ui()
-
+    `;
+  maindiv.appendChild(div);
+  return maindiv;
+};
+const insertToDom = async () => {
+  let results = await fetch('https://api.tvmaze.com/shows');
+  results = await results.json();
+  results.forEach((element) => {
+    container.appendChild(populateUI(element));
+  });
+};
+insertToDom();
