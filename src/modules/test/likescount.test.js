df2d56a5a0f95ff56b4c2513a57feecce2a4e38a
LdouglasOT <douglas.luzinda.ot@gmail.com>
2023-02-11 09:21:37 -0500

    fix likes tests

--- src/modules/test/likescount.test.js
+++ src/modules/test/likescount.test.js
@@ -1,25 +1,24 @@
-import countAllLikes from "../LikesCount";
+import countAllLikes from "../Likescount";
 import { spyOn } from 'jest-mock';
-
-const fetchSpy = spyOn(global, 'fetch');
+global.fetch = jest.fn();
 
 describe('countAllLikes', () => {
   beforeEach(() => {
-    fetchSpy.mockReset();
+    fetch.mockReset();
   });
 
   test('returns the number of likes', async () => {
     const likes = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    fetchSpy.mockResolvedValue({ json: () => Promise.resolve(likes) });
+    fetch.mockResolvedValue({ json: () => Promise.resolve(likes) });
     const result = await countAllLikes();
-    expect(result.length).toBe(3);
+    expect(result).toBe(3);
   });
 
   test('returns the likes object', async () => {
     const likes = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    fetchSpy.mockResolvedValue({ json: () => Promise.resolve(likes) });
+    fetch.mockResolvedValue({ json: () => Promise.resolve(likes) });
     const result = await countAllLikes();
-    expect(result).toEqual(likes);
+    expect(result).toBe(3);
   });
 });
 
